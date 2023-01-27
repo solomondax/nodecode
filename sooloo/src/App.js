cffbaccc070c773aa90d6e3909bdd0251df4c3a8
solomonp <solodax46@gamil.com>
2023-01-27 16:44:42 +0530

    an

--- sooloo/src/App.js
+++ sooloo/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload dax riderh.
+          Edit <code>src/App.js</code> and save to reload dax riderh solo.
         </p>
         <a
           className="App-link"
